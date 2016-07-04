262ac46cf12196349fcee42fcf7b6308da37075f
ComradeGrey <ilyareznik91@gmail.com>
2016-07-04 22:52:12 +0300

    first commit

---
+++ src/com/palkagames/engine/Game.java
@@ -0,0 +1,13 @@
+package com.palkagames.engine;
+
+/**
+ * Created by Zver on 04.07.2016.
+ */
+public class Game {
+    //длина игрового экрана
+    public static final int width = 800;
+    //высота игрового экрана
+    public static final int height = width / 16 * 9;
+    //масштабирование?
+    public static final int scale = 3;
+}
